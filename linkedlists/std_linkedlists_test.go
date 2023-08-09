663b9c0d6faf32b912c358c1d77783038e89936e
Lyndsey Ferguson <lyndsey-ferguson@users.noreply.github.com>
2023-08-09 08:34:14 -0400

    Clean up linkedlists tests (#5)
    * Clean up linkedlists tests
    
    * fixup! Clean up linkedlists tests

--- linkedlists/std_linkedlists_test.go
+++ linkedlists/std_linkedlists_test.go
@@ -1,40 +1,43 @@
 package linkedlists
 
 import (
+	"strconv"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
+
+func convertLinkedListToString(list *Node) string {
+	var result string
+	for cursor := list; cursor != nil; {
+		result = result + strconv.Itoa(cursor.data)
+		cursor = cursor.next
+		if cursor != nil {
+			result = result + ", "
+		}
+	}
+	return result
+}
 
 func TestBasicNodeCreation(t *testing.T) {
 	node := CreateNode(1)
-	if node.data != 1 {
-		t.Errorf("node.data = %d; want 1", node.data)
-	}
+	assert.Equal(t, 1, node.data)
+	assert.Equal(t, "1", convertLinkedListToString(node))
 }
 
 func TestAppendNodeToList(t *testing.T) {
 	list := CreateNode(1)
 	node := CreateNode(2)
 	list = AppendNode(list, node)
-	if list.next != node {
-		t.Errorf("list.next = %x; want %x", &list.next, &node)
-	}
-	if list.data != 1 {
-		t.Errorf("list.data = %d; want %d", list.data, 1)
-	}
-	if list.next.data != 2 {
-		t.Errorf("list.next.data = %d; want %d", list.next.data, 2)
-	}
+	assert.Equal(t, node, list.next)
+	assert.Equal(t, "1, 2", convertLinkedListToString(list))
 }
 
 func TestAppendNodeToEmptyList(t *testing.T) {
 	node := CreateNode(2)
 	list := AppendNode(nil, node)
-	if list != node {
-		t.Errorf("list = %x; want %x", &list.next, &node)
-	}
-	if list.data != 2 {
-		t.Errorf("list.data = %d; want %d", list.data, 2)
-	}
+	assert.Equal(t, node, list)
+	assert.Equal(t, "2", convertLinkedListToString(list))
 }
 
 func TestAppendToTwoElementList(t *testing.T) {
@@ -44,21 +47,9 @@ func TestAppendToTwoElementList(t *testing.T) {
 	list = AppendNode(list, node2)
 	list = AppendNode(list, node3)
 
-	if list.next != node2 {
-		t.Errorf("list.next = %x; want %x", &list.next, &node2)
-	}
-	if list.next.next != node3 {
-		t.Errorf("list.next.next = %x; want %x", &list.next, &node3)
-	}
-	if list.data != 1 {
-		t.Errorf("list.data = %d; want %d", list.data, 1)
-	}
-	if list.next.data != 2 {
-		t.Errorf("list.data = %d; want %d", list.data, 2)
-	}
-	if list.next.next.data != 3 {
-		t.Errorf("list.data = %d; want %d", list.data, 3)
-	}
+	assert.Equal(t, node2, list.next)
+	assert.Equal(t, node3, list.next.next)
+	assert.Equal(t, "1, 2, 3", convertLinkedListToString(list))
 }
 
 func TestInsertAfterNode(t *testing.T) {
@@ -70,18 +61,8 @@ func TestInsertAfterNode(t *testing.T) {
 	node4 := CreateNode(4)
 
 	InsertAfter(node2, node4)
-	if list.data != 1 {
-		t.Errorf("list.data = %d; want %d", list.data, 1)
-	}
-	if list.next.data != 2 {
-		t.Errorf("list.next.data = %d; want %d", list.data, 2)
-	}
-	if list.next.next.data != 4 {
-		t.Errorf("list.next.next.data = %d; want %d", list.data, 4)
-	}
-	if list.next.next.next.data != 3 {
-		t.Errorf("list.next.next.next.data = %d; want %d", list.data, 3)
-	}
+
+	assert.Equal(t, "1, 2, 4, 3", convertLinkedListToString(list))
 }
 
 func TestRemoveAfter(t *testing.T) {
@@ -92,12 +73,7 @@ func TestRemoveAfter(t *testing.T) {
 	list = AppendNode(list, node3)
 
 	RemoveAfter(list)
-	if list.data != 1 {
-		t.Errorf("list.data = %d; want %d", list.data, 1)
-	}
-	if list.next.data != 3 {
-		t.Errorf("list.next.data = %d; want %d", list.next.data, 3)
-	}
+	assert.Equal(t, "1, 3", convertLinkedListToString(list))
 }
 
 // taken from
@@ -109,13 +85,11 @@ func TestSearchNodeIteratively(t *testing.T) {
 	list = AppendNode(list, node3)
 
 	foundNode := SearchNodeIteratively(list, 2)
-	if foundNode.data != 2 {
-		t.Errorf("foundNode.data = %d; want %d", foundNode.data, 2)
-	}
+	assert.Equal(t, node2, foundNode)
+	assert.Equal(t, 2, foundNode.data)
+
 	foundNode = SearchNodeIteratively(list, 12)
-	if foundNode != nil {
-		t.Errorf("foundNode is %x; want nil", &foundNode)
-	}
+	assert.Nil(t, foundNode)
 }
 
 func TestSearchNodeRecursively(t *testing.T) {
