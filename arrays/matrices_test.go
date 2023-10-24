4c77a54fb1216e6cf37e58ac82f964c79bfa89ec
Lyndsey Ferguson <lyndsey.ferguson@appian.com>
2023-10-24 12:57:45 -0400

    zero out an axis in matrix

--- arrays/matrices_test.go
+++ arrays/matrices_test.go
@@ -81,3 +81,43 @@ func TestRotateImage90DegressInPlace2(t *testing.T) {
 		}
 	}
 }
+
+func TestZeroOutAxisInMatrix(t *testing.T) {
+	m1 := [][]int{
+		{1, 2, 3},
+		{4, 0, 6},
+		{7, 8, 9},
+	}
+
+	expected_m1_result := [][]int{
+		{1, 0, 3},
+		{0, 0, 0},
+		{7, 0, 9},
+	}
+
+	zeroOutAxisInMatrix(m1)
+	for r := 0; r < len(expected_m1_result); r++ {
+		for c := 0; c < len(expected_m1_result[0]); c++ {
+			assert.Equal(t, expected_m1_result[r][c], m1[r][c])
+		}
+	}
+
+	m2 := [][]int{
+		{0, 2, 0},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+
+	expected_m2_result := [][]int{
+		{0, 0, 0},
+		{0, 5, 0},
+		{0, 8, 0},
+	}
+
+	zeroOutAxisInMatrix(m2)
+	for r := 0; r < len(expected_m2_result); r++ {
+		for c := 0; c < len(expected_m2_result[0]); c++ {
+			assert.Equal(t, expected_m2_result[r][c], m2[r][c])
+		}
+	}
+}
