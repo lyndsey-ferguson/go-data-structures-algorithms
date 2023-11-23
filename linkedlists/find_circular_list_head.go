138375f8866bbfaa1a0f7b35722ddfc59bfd0b2c
Lyndsey Ferguson <lyndsey.ferguson@appian.com>
2023-11-23 11:40:11 -0500

    find head of a loop in linked list using runners

--- linkedlists/find_circular_list_head.go
+++ linkedlists/find_circular_list_head.go
@@ -12,3 +12,47 @@ func findCircularListHeadWithHash[T comparable](list *Node[T]) *Node[T] {
 	}
 	return nil
 }
+
+func findCircularListHeadWithRunners[T comparable](list *Node[T]) *Node[T] {
+
+	if list == nil || list.next == nil {
+		return nil
+	}
+
+	fastRunner := list
+	slowRunner := list
+
+	// find node where the runners collide
+	for fastRunner != nil && fastRunner.next != nil {
+		fastRunner = fastRunner.next.next
+		slowRunner = slowRunner.next
+
+		if slowRunner == fastRunner {
+			break
+		}
+	}
+
+	// exit early if the fastRunner made it to the end of the list
+	if fastRunner == nil || fastRunner.next == nil {
+		return nil
+	}
+
+	// now, the fastRunner and slowRunner have collided, and
+	// according to their natures where fastRunner moves at
+	// 2 nodes per iteration while slowRunner moves at 1 node
+	// per iteration, AND fastRunner had a head start of "k"
+	// nodes into the loop, where k is the number of nodes
+	// for slowRunner to reach the loop, fastRunner will
+	// have caught up to slowRunner in loopsize - k steps
+	// leaving both k nodes before the beginning of the loop
+	// (because fastRunner started k nodes into the loop, and
+	// there will be loopsize - k nodes left). This allows
+	// us to reset slowRunner to the head of the list
+	// and then step each runner at the speed of 1 node per
+	// iteration until they collide again
+	slowRunner = list
+	for ; slowRunner != fastRunner; slowRunner, fastRunner = slowRunner.next, fastRunner.next {
+	}
+
+	return slowRunner
+}
--- linkedlists/find_circular_list_head_test.go
+++ linkedlists/find_circular_list_head_test.go
@@ -14,10 +14,40 @@ func TestFindCircularListHeadWithHash(t *testing.T) {
 	AppendNode(list, cNode)
 
 	assert.Equal(t, cNode, findCircularListHeadWithHash(list))
+
+	list = createLetterList([]string{"A", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"})
+	dNode := findNode[string](list, func(n *Node[string]) bool {
+		return n.data == "D"
+	})
+	AppendNode(list, dNode)
+	assert.Equal(t, dNode, findCircularListHeadWithHash(list))
 }
 
 func TestFindCircularListHeadWithHashReturnsNil(t *testing.T) {
 	list := createLetterList([]string{"A", "B", "C", "D", "E"})
 
 	assert.Nil(t, findCircularListHeadWithHash(list))
+}
+
+func TestFindCircularListHeadWithRunners(t *testing.T) {
+	list := createLetterList([]string{"A", "B", "C", "D", "E"})
+	cNode := findNode[string](list, func(n *Node[string]) bool {
+		return n.data == "C"
+	})
+	AppendNode(list, cNode)
+
+	assert.Equal(t, cNode, findCircularListHeadWithRunners(list))
+
+	list = createLetterList([]string{"A", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"})
+	dNode := findNode[string](list, func(n *Node[string]) bool {
+		return n.data == "D"
+	})
+	AppendNode(list, dNode)
+	assert.Equal(t, dNode, findCircularListHeadWithRunners(list))
+}
+
+func TestFindCircularListHeadWithRunnersReturnsNil(t *testing.T) {
+	list := createLetterList([]string{"A", "B", "C", "D", "E"})
+
+	assert.Nil(t, findCircularListHeadWithRunners(list))
 }
