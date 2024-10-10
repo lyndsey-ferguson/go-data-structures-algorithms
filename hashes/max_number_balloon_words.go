0d048de52e8014cfa041d0f65181628e9ab20da8
Lyndsey Ferguson <lyndsey.ferguson@appian.com>
2024-10-10 08:54:12 -0400

    max number word 'balloon' appears in string

---
+++ hashes/max_number_balloon_words.go
@@ -0,0 +1,40 @@
+package hashes
+
+import (
+	"math"
+)
+
+type Solution struct{}
+
+func minInt(ints ...int) (min int) {
+	min = math.MaxInt32
+	for _, i := range ints {
+		if i < min {
+			min = i
+		}
+	}
+	return
+}
+
+func maxNumberOfBalloons(text string) int {
+	hash := make(map[rune]int, 7)
+	hash['b'] = 0
+	hash['a'] = 0
+	hash['l'] = 0
+	hash['o'] = 0
+	hash['n'] = 0
+
+	for _, r := range text {
+		hash[r]++
+	}
+
+	min := minInt(
+		hash['b'],
+		hash['a'],
+		hash['l']/2,
+		hash['o']/2,
+		hash['n'],
+	)
+
+	return min
+}
---
+++ hashes/max_number_balloon_words_test.go
@@ -0,0 +1,28 @@
+package hashes
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMaxNumberOfBalloons(t *testing.T) {
+	// "balloonballoon" -> 2
+	// "bbaall" -> 0
+	// "balloonballoooon" -> 2
+	// "balloonnolab" -> 1
+
+	type BalloonSample struct {
+		word          string
+		expectedCount int
+	}
+	testCases := []BalloonSample{
+		{"balloonballoon", 2},
+		{"bbaall", 0},
+		{"balloonballoooon", 2},
+		{"balloonnolab", 1},
+	}
+	for _, tc := range testCases {
+		assert.Equal(t, tc.expectedCount, maxNumberOfBalloons(tc.word))
+	}
+}
